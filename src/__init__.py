a0e891d2fb96262d2e1d3516c1d67b127ed8e091
Ícaro Bruno <54824248+icarob-eng@users.noreply.github.com>
2021-05-09 11:18:41 -0300

    Doc em __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
+"""
+Capym: capy, python, mecânica
+=====================
+Capym tem ferramentas par fazer simulações mecânicas (no momento apenas simulações gravitacionais).
+A documentação e variáveis são todas em português :), exceto itens de outras bibliotecas.
+Futuruamente, serão adicionados recursos para simulações tridimensionais, objetos que colidam, corpos rígidos, etc.
+Nos módulos existem alguns exemplos de simulações interessantes.
+
+Módulos
+-------
+sim
+    Ferramentas para fazer as mecânicas da simulação assim como declarar objetos das simulações.
+ani
+    Funções para animar e salvar as simulações feitas em sim. Para salvar a simulação se necessta ter instalado ffmpeg.
+
+"""
+
 __all__ = ['sim', 'ani']
