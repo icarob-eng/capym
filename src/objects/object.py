02cf5b1a6587d27c9300742797cbd6b196c47e20
icarob-eng <ibsc013@gmail.com>
2023-12-12 14:59:55 -0300

    Define linha

--- src/objects/object.py
+++ src/objects/object.py
@@ -1,7 +1,6 @@
-import numpy
+import numpy as np
 import uuid
 
-from numpy import array
 from loguru import logger
 from attrs import define, field
 
@@ -10,20 +9,20 @@ from attrs import define, field
 class Object(object):
     uuid: uuid = field(default=uuid.uuid4(), init=False)
     mass: float
-    position: array
-    velocity: array
+    position: np.array
+    velocity: np.array
 
-    def acceleration_contribution(self, other, gravitational_constant: float, **kwargs) -> array:
+    def acceleration_contribution(self, other, gravitational_constant: float, **kwargs) -> np.array:
         return self.__gravitational_acceleration(other, gravitational_constant)
 
     @logger.catch
-    def __gravitational_acceleration(self, other: 'Object', gravitational_constant: float) -> array:
+    def __gravitational_acceleration(self, other: 'Object', gravitational_constant: float) -> np.array:
         distance_vector = other.position - self.position
 
-        if (numpy.linalg.norm(distance_vector)) == 0:
+        if (np.linalg.norm(distance_vector)) == 0:
             raise Exception('Two objects can\'t be in the same position')
 
-        return (gravitational_constant * other.mass / numpy.linalg.norm(distance_vector)
+        return (gravitational_constant * other.mass / np.linalg.norm(distance_vector)
                 *
-                distance_vector / numpy.linalg.norm(distance_vector))
+                distance_vector / np.linalg.norm(distance_vector))
         # Gm/|r| * r/|r|
